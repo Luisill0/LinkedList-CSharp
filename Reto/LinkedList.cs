80a648ba36597da2825b46caf8cf49048c24f099
Luis Naranjo <luise_nc@outlook.com>
2021-03-02 17:28:35 -0600

    First Commit

---
+++ Reto/LinkedList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reto
+{
+    public class LinkedList
+    {
+        public enum Position
+        {
+            BEFORE,AFTER
+        }
+
+        private Node head;
+        private Node tail;
+        private int size;
+
+        /***
+         * Inserts data at the end of the list
+         * @param data Data to be inserted
+        */
+        public void add(int data)
+        {
+            Node node = new Node(data);
+            node.setPrevious(tail);
+
+            if (tail != null)
+            {
+                tail.setNext(node);
+            }
+
+            if (head == null)
+            {
+                head = node;
+            }
+
+            tail = node;
+            size++;
+        }
+
+        /***
+         * @param index 0-index
+         * @return data in index
+        */
+        public int get(int index)
+        {
+            Node currentNode = head;
+            int currentIndex = 0;
+            while (currentIndex < index)
+            {
+                currentNode = currentNode.getNext();
+                currentIndex++;
+            }
+
+            return currentNode.getData();
+        }
+
+        /***
+         * @param index 0-index
+        */
+        public void delete(int index)
+        {
+            Node currentNode = head;
+            int currentIndex = 0;
+
+            if (index < 0 || index >= size)
+            {
+                return;
+            }
+            size--;
+
+            if (size == 0)
+            {
+                head = head.getNext();
+                head.setPrevious(null);
+            }
+
+            if (index == size)
+            {
+                tail = tail.getPrevious();
+                tail.setNext(null);
+            }
+
+            if (index > 0 && index < size)
+            {
+                while (currentIndex < index)
+                {
+                    currentNode = currentNode.getNext();
+                    currentIndex++;
+                }
+                currentNode.getPrevious().setNext(currentNode.getNext());
+                currentNode.getNext().setPrevious(currentNode.getPrevious());
+            }
+        }
+
+        /***
+         * 
+         */
+        public void insert(int data, Position pos, Iterator it)
+        {
+            Node newNode = new Node(data);
+            Node currentNode = it.getCurrentNode();
+
+            if(pos == Position.AFTER)
+            {
+                newNode.setNext(currentNode.getNext());
+                newNode.setPrevious(currentNode);
+                currentNode.setNext(newNode);
+                if(newNode.getNext() != null)
+                {
+                    newNode.getNext().setPrevious(newNode);
+                }
+                else
+                {
+                    tail = newNode;
+                }
+            }
+            else
+            {
+                newNode.setPrevious(currentNode.getPrevious());
+                newNode.setNext(currentNode);
+                currentNode.setPrevious(newNode);
+                if (newNode.getPrevious() != null)
+                {
+                    newNode.getPrevious().setNext(newNode);
+                }
+                else
+                {
+                    head = newNode;
+                }
+            }
+            size++;
+        }
+
+        public Iterator getIterator()
+        {
+            return new Iterator(head);
+        }
+
+        public ReverseIterator getReverseIterator()
+        {
+            return new ReverseIterator(tail);
+        }
+
+        public int getSize()
+        {
+            return size;
+        }
+    }
+}
---
+++ Reto/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Reto
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList lista1 = new LinkedList();
+
+            lista1.add(1);
+            lista1.add(4);
+            lista1.add(3);
+            lista1.add(7);
+
+            Iterator it = lista1.getIterator();
+
+            while (it.hasNext())
+            {
+                Iterator backupIt = new Iterator(it);
+                int element = it.next();
+
+                if (element == 3)
+                {
+                    lista1.insert(10, LinkedList.Position.BEFORE, backupIt);
+                }
+
+                if(element == 7)
+                {
+                    lista1.insert(15, LinkedList.Position.AFTER, backupIt);
+                }
+            }
+
+            lista1.delete(3);
+
+            Console.WriteLine("El tamaño es: {0}", lista1.getSize());
+            Console.WriteLine("------------------------------------");
+
+            it = lista1.getIterator();
+
+            while (it.hasNext())
+            {
+                int element = it.next();
+                Console.WriteLine("Dato: {0}", element);
+            }
+
+            Console.WriteLine("------------------------------------");
+
+            ReverseIterator reverseIterator = lista1.getReverseIterator();
+
+            while (reverseIterator.hasNext())
+            {
+                int element = reverseIterator.next();
+                Console.WriteLine("Dato: {0}", element);
+            }
+            Console.WriteLine("------------------------------------");
+        }
+    }
+}
